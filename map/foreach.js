fbd857054dd21163e39eb3330cdeea1e958d8593
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-07-22 17:22:29 +0530

    for each

---
+++ map/foreach.js
@@ -0,0 +1,8 @@
+///does not return anything
+
+const numbers = [1, 2, 3, 4, 5];
+
+// forEach ka istemal karke har element ko double karke console me print karte hain
+numbers.forEach(function(number) {
+    console.log(number * 2);
+});
